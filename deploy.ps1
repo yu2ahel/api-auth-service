9b573fdb474afbf059cd791bfa8be877c63c197e
Hazem Safwat <HSafwat@moj.gov.sa>
2024-06-19 05:41:27 +0300

    Add build step to deployment script

--- deploy.ps1
+++ deploy.ps1
@@ -1,3 +1,5 @@
-npm version patch
+$newVersion = npm version patch
+$envContent = Get-Content -Path .\.env
+$updatedEnvContent = $envContent -replace '(APP_VERSION=).*', "`$1$newVersion"
 git push
-ssh pinkstaging2 "cd /var/www/node_apps/api-auth-service && git pull origin master && npm install && npm run build && /home/ubuntu/.nvm/versions/node/v21.6.1/bin/pm2 restart api-auth-service"
+ssh pinkstaging2 "cd /var/www/node_apps/api-auth-service && git pull origin master  && npm install && npm run build && /home/ubuntu/.nvm/versions/node/v21.6.1/bin/pm2 restart api-auth-service"
