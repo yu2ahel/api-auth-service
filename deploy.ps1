49a9c52e891cfd50fad634313bd850513346ad50
Hazem Safwat <HSafwat@moj.gov.sa>
2024-06-19 04:44:29 +0300

    Add build step to deployment script

--- deploy.ps1
+++ deploy.ps1
@@ -1,1 +1,1 @@
-ssh pinkstaging2 "cd /var/www/node_apps/api-auth-service && git pull origin master && npm install && && npm run build && /home/ubuntu/.nvm/versions/node/v21.6.1/bin/pm2 restart api-auth-service"
+ssh pinkstaging2 "cd /var/www/node_apps/api-auth-service && git pull origin master && npm install && npm run build && /home/ubuntu/.nvm/versions/node/v21.6.1/bin/pm2 restart api-auth-service"
