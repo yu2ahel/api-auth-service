33bae5b6193d61ed91179e59ec9705956a7d5552
Hazem Safwat <HSafwat@moj.gov.sa>
2024-06-19 06:06:53 +0300

    "Remove unnecessary echo commands from deploy script"

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { config } from 'dotenv';
 
 config();
-process.env.APP_VERSION = 'version';
 import * as swaggerUi from 'swagger-ui-express';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
