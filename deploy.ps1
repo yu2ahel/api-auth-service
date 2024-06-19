e37322b90caee2a4c3923420e3e5899dc1f6dc76
Hazem Safwat <HSafwat@moj.gov.sa>
2024-06-19 06:01:15 +0300

    Add build step to deployment script

--- deploy.ps1
+++ deploy.ps1
@@ -1,8 +1,17 @@
 $newVersion = npm version patch
-$envContent = Get-Content -Path .\.env
-echo $newVersion
-$updatedEnvContent = $envContent -replace '(APP_VERSION=).*', "`$1$newVersion"
-echo $updatedEnvContent
-$updatedEnvContent | Set-Content -Path .\.env
+
+
+
+# if $newVersion not empty do the next line
+if (!([string]::IsNullOrEmpty($newVersion)))
+{
+    $envContent = Get-Content -Path .\.env
+    echo $newVersion
+    $updatedEnvContent = $envContent -replace '(APP_VERSION=).*', "`$1$newVersion"
+    echo $updatedEnvContent
+    $updatedEnvContent | Set-Content -Path .\.env
+    ssh pinkstaging2 "cd /var/www/node_apps/api-auth-service &&  sed -i 's/\(APP_VERSION=\).*/\1$newVersion/' .env"
+}
+
 git push
-ssh pinkstaging2 "cd /var/www/node_apps/api-auth-service && git pull origin master  && npm install && sed -i 's/\(APP_VERSION=\).*/\1$newVersion/' .env && npm run build && /home/ubuntu/.nvm/versions/node/v21.6.1/bin/pm2 restart api-auth-service"
+ssh pinkstaging2 "cd /var/www/node_apps/api-auth-service && git pull origin master  && npm install && npm run build && /home/ubuntu/.nvm/versions/node/v21.6.1/bin/pm2 restart api-auth-service"
